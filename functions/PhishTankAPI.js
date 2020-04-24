9db3eaa387cbec4da76eb1e92f2f5886887e7001
Nicolás Campuzano <nicolas.campuzano@tulpep.com>
2020-04-24 10:23:40 -0500

    Create function to get data from phishtank

--- functions/PhishTankAPI.js
+++ functions/PhishTankAPI.js
@@ -1,5 +1,33 @@
+import { Constants } from 'expo';
 const BASE_URL = 'https://checkurl.phishtank.com/checkurl/index.php';
 
-async function GetURLInformation(url) {
 
+async function GetURLInformation(url) {
+  try {
+    const parameters = {
+      url,
+      format: 'json',
+      app_key: Constants.manifest.extra.PhishTankKey
+    }
+    let formData = new FormData();
+
+    for (let k in parameters) {
+      formData.append(k, parameters[k]);
+    }
+
+    let response = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'multipart/form-data',
+      },
+      body: formData
+    });
+    console.log(response);
+    return response;
+  } catch(error) {
+    console.error(error);
+    return null;
+  }
 }
+export { GetURLInformation };
